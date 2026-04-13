014b3064b5319f5c7fa15347be00067a5f989610
rudra2609-06 <rupalthakkar18587@gmail.com>
2026-04-13 09:25:36 +0530

    fixed url issue

--- frontend/src/lib/apiInstance.js
+++ frontend/src/lib/apiInstance.js
@@ -4,7 +4,7 @@ const apiInstance = axios.create({
   baseURL:
     import.meta.env.VITE_MODE === "dev"
       ? import.meta.env.VITE_BACKEND_URL_DEVELOPMENT_WITH_ENDPOINT
-      : "/api",
+      : import.meta.env.VITE_BACKEND_URL_WITH_ENDPOINT,
   withCredentials: true,
 });
 
